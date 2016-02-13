c5d2fc4d61ae61491ea8b7d3974a5c02be6f7233
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2016-02-13 12:41:03 -0600

    moved js scripts in head.html to assets/js/

---
+++ assets/js/mathjax_config.js
@@ -0,0 +1,18 @@
+
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [ ['$','$'], ],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  TeX: {
+    Macros: {
+      nhi : "N(HI)",
+      htwo : "H_2",
+      in : "^{\\prime\\prime}",
+      ft : "^{\\prime}",
+      deg : "^\\circ",
+    }
+  }
+});
+
