a756c30444d820b51a12476776d90bf75eab15f7
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2016-02-14 13:11:05 -0600

    examples and listings now independent classes, both can be accessed by js on same page

--- assets/js/fa-expand-all-open.js
+++ assets/js/fa-expand-all-open.js
@@ -1,13 +1,19 @@
 
 $(document).ready(function() {
-    $(".page").children().find(".page-item-body").css( "display", "block" );
-    $(".page").children().find(".pib-indicator")
+    $(".page").children().find(".page-item-body-example").css( "display", "block" );
+    $(".page").children().find(".pib-indicator-example")
         .find("i").toggleClass("fa-compress").toggleClass("fa-expand");
 
-    $(".page-item .pib-indicator").click(function() {
+    $(".page-item .pib-indicator-listing").click(function() {
         var preview = $(this);
         preview.find("i").toggleClass("fa-expand").toggleClass("fa-compress");
-        $(this).closest(".page-item").find(".page-item-body").toggle("slow", function() {});
+        $(this).closest(".page-item").find(".page-item-body-listing").toggle("slow", function() {});
+    });
+
+    $(".page-item .pib-indicator-example").click(function() {
+        var preview = $(this);
+        preview.find("i").toggleClass("fa-expand").toggleClass("fa-compress");
+        $(this).closest(".page-item").find(".page-item-body-example").toggle("slow", function() {});
     });
 });
 
