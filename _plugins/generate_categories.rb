78ddcfca63c7882473cb94d6a227b25c06532103
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2018-01-28 22:32:29 +0000

    pagination of only professional

--- _plugins/generate_categories.rb
+++ _plugins/generate_categories.rb
@@ -72,7 +72,6 @@ module Jekyll
       # Set the meta-description for this page.
       meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
       self.data['description'] = "#{meta_description_prefix}#{category}"
-      puts self
     end
 
   end
